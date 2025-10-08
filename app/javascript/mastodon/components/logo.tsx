53649716cb33e98d541c31d60eb211d23cb35d19
vdyotte <vdyotte@gmail.com>
2025-10-08 15:57:58 -0400

    merge Qlub back into 4.4.5

--- app/javascript/mastodon/components/logo.tsx
+++ app/javascript/mastodon/components/logo.tsx
@@ -1,19 +1,19 @@
 import logo from '@/images/logo.svg';
 
 export const WordmarkLogo: React.FC = () => (
-  <svg viewBox='0 0 261 66' className='logo logo--wordmark' role='img'>
-    <title>Mastodon</title>
+  <svg viewBox='0 0 199.85 66' className='logo logo--wordmark' role='img'>
+    <title>Qlub</title>
     <use xlinkHref='#logo-symbol-wordmark' />
   </svg>
 );
 
 export const IconLogo: React.FC = () => (
   <svg viewBox='0 0 79 79' className='logo logo--icon' role='img'>
-    <title>Mastodon</title>
+    <title>Qlub</title>
     <use xlinkHref='#logo-symbol-icon' />
   </svg>
 );
 
 export const SymbolLogo: React.FC = () => (
-  <img src={logo} alt='Mastodon' className='logo logo--icon' />
+  <img src={logo} alt='Qlub' className='logo logo--icon' />
 );
--- app/javascript/mastodon/features/about/index.jsx
+++ app/javascript/mastodon/features/about/index.jsx
@@ -125,7 +125,7 @@ class About extends PureComponent {
 
           <RulesSection />
 
-          <Section title={intl.formatMessage(messages.blocks)} onOpen={this.handleDomainBlocksOpen}>
+          {/* <Section title={intl.formatMessage(messages.blocks)} onOpen={this.handleDomainBlocksOpen}>
             {domainBlocks.get('isLoading') ? (
               <>
                 <Skeleton width='100%' />
@@ -154,7 +154,7 @@ class About extends PureComponent {
             ) : (
               <p><FormattedMessage id='about.not_available' defaultMessage='This information has not been made available on this server.' /></p>
             ))}
-          </Section>
+          </Section> */}
 
           <LinkFooter />
 
